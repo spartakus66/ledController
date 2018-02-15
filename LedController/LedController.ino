d2b6285f2e5d0e8ef57de4e623b76a061a2333ab
Krzysiek <krzysiotonie@gmail.com>
2018-02-15 10:50:57 +0100

    dodane Led.h i LedPool.h

--- LedController/LedController.ino
+++ LedController/LedController.ino
@@ -5,19 +5,13 @@
 */
 
 #include "CircularBuffer.h"
-
-#define LED_BAR_1 57
-#define LED_BAR_2 58
-#define LED_BAR_3 59
-#define LED_BAR_4 60
-#define LED_BAR_10 37
-#define LED_BAR_9 38
-#define LED_BAR_8 39
-#define LED_BAR_7 40
-#define LED_BAR_6 41
-#define LED_BAR_5 42
+#include "IGpio.h"
+#include "Led.h"
+#include "LedPool.h"
 
 CircularBuffer *receiveBuffer;
+
+
 
 // the setup function runs once when you press reset or power the board
 void setup() 
@@ -27,19 +21,11 @@ void setup()
 	{
 		;
 	}
-	pinMode(LED_BAR_1, OUTPUT);
-	pinMode(LED_BAR_2, OUTPUT);
-	pinMode(LED_BAR_3, OUTPUT);
-	pinMode(LED_BAR_4, OUTPUT);
-	pinMode(LED_BAR_5, OUTPUT);
-	pinMode(LED_BAR_6, OUTPUT);
-	pinMode(LED_BAR_7, OUTPUT);
-	pinMode(LED_BAR_8, OUTPUT);
-	pinMode(LED_BAR_9, OUTPUT);
-	pinMode(LED_BAR_10, OUTPUT);
-	digitalWrite(LED_BAR_1, HIGH);
 
-	receiveBuffer = new CircularBuffer(450);
+
+
+
+	receiveBuffer = new CircularBuffer(200);
 
 }
 
@@ -48,13 +34,14 @@ void loop()
 {
 	static unsigned long previousMillis = 0;
 	unsigned long currentMillis = millis();
-	long interval = 500;
+	long interval = 3000;
 
 	if (currentMillis - previousMillis >= interval) 
 	{
 		// save the last time you blinked the LED
 		previousMillis = currentMillis;
-		digitalWrite(LED_BAR_10, !digitalRead(LED_BAR_10));
+		digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
 	}
 
 }
